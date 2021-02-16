b141564fefdad31858ce7f88227eb71929e27eef
Francisco Dadone <49202617+FranciscoDadone@users.noreply.github.com>
2021-02-15 21:11:55 -0300

    Add files via upload

--- scrap.py
+++ scrap.py
@@ -3,6 +3,16 @@ import csv
 import sys
 from unidecode import unidecode
 import getpass 
+import webbrowser
+
+# MacOS
+#chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+
+# Windows
+# chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+
+# Linux
+chrome_path = '/usr/bin/google-chrome %s'
 
 
 L = instaloader.Instaloader()
@@ -80,26 +90,52 @@ elif displayType == 2:
     toDisplay = profile.get_followees()
     print("Loading following list...")
 
+urls = []
 i = 0
+elegible = 0
 for followee in toDisplay:
     user = unidecode(followee.full_name.upper() or followee.username.upper()).replace('.', '').replace('-','').replace('(','').replace(')','').replace('@','').replace('[','').replace(']','').replace('_', ' ').replace("'", '').split(' ', 1)[0].replace('_', '').replace('~', '').replace('!', '').replace('*', '').replace('0', '').replace('1', '').replace('2', '').replace('3', '').replace('4', '').replace('5', '').replace('6', '').replace('7', '').replace('8', '').replace('9', '').replace('#', '').strip()
     i+=1
     
-    #if followee.followers <= followerLimit:
     if maleOrFemale == 1 and isFemale(user):
         print("(",str(i),")    ", (followee.full_name or followee.username), "               (https://www.instagram.com/"+followee.username+")")
+        urls.append(followee)
+        elegible+=1
     elif maleOrFemale == 2:
+        urls.append(followee)
         if isFemale(user):
             print("(",str(i),")  (fem)  ", (followee.full_name or followee.username), "               (https://www.instagram.com/"+followee.username+")")
         elif not isMale(user):
             print("(",str(i),")  (unk)  ", (followee.full_name or followee.username), "               (https://www.instagram.com/"+followee.username+")          ", user)
+        elegible+=1
     elif maleOrFemale == 3 and isMale(user):
+        urls.append(followee)
         print("(",str(i),")    ", (followee.full_name or followee.username), "               (https://www.instagram.com/"+followee.username+")")
+        elegible+=1
     elif maleOrFemale == 4:
+        urls.append(followee)
         if isMale(user):
             print("(",str(i),")  (man)  ", (followee.full_name or followee.username), "               (https://www.instagram.com/"+followee.username+")")
         elif not isFemale(user):
             print("(",str(i),")  (unk)  ", (followee.full_name or followee.username), "               (https://www.instagram.com/"+followee.username+")          ", user)
+        elegible+=1
     elif maleOrFemale == 5 and not isFemale(user) and not isMale(user):
+        urls.append(followee)
         print("(",str(i),")  (unk)  ", (followee.full_name or followee.username), "               (https://www.instagram.com/"+followee.username+")          ", user)
-        
+        elegible+=1
+
+print(elegible, "profiles to be opened...")
+tabs_n = input("How many tabs to open in the browser time at a time? ")
+j = 0
+tabsOpened = 0
+for i in urls:
+    j+=1
+    
+    url = "https://www.instagram.com/"+i.username
+    if not i.is_verified:
+        webbrowser.get(chrome_path).open(url)
+        tabsOpened+=1
+    if j == int(tabs_n):
+        input("Continue? (Enter)")
+        j = 0
+print("Total tabs opened:", tabsOpened)
